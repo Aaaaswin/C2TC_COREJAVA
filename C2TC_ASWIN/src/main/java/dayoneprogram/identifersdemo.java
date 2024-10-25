f9b1fb6d4776e57ca2bc3aa1c6f5e2c1c0ba6d4d
aswin <aswin@Aswin>
2024-10-25 14:53:08 +0530

    identified demo

---
+++ C2TC_ASWIN/src/main/java/dayoneprogram/identifersdemo.java
@@ -0,0 +1,13 @@
+package dayoneprogram;
+
+public class identifersdemo {
+
+	public static void main(String[] args) {
+		int  num=109;
+		System.out.println("value of the numbre variable is"+ num);
+		String studname="Aswin";
+		System.out.println("Name:"+ studname);
+
+	}
+
+}
